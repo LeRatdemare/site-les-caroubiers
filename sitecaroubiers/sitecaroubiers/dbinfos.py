3c4ecbc1972c520af6a8f5749f2031125c503cb1
Nathan Lufuluabo <lufuluabon@outlook.fr>
2023-07-16 20:53:12 +0200

    Initialisation du site et de la db MySQL

---
+++ sitecaroubiers/sitecaroubiers/dbinfos.py
@@ -0,0 +1,11 @@
+from MySQLdb import _mysql
+
+host = "localhost"
+port = 3306
+user = "unecaroube"
+password = "caroubesdu26"
+db_name = "caroubiers_db"
+# db = _mysql.connect(host=host, port=port, user=user, password=password, database=db_name)
+# db.query("CREATE DATABASE "+db_name)
+# db.close()
+# db.query("""SELECT spam, eggs, sausage FROM breakfast WHERE price < 5""")
