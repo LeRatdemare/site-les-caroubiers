38da61c6916daafde8885ed5208610698cad586f
Nathan Lufuluabo <lufuluabon@outlook.fr>
2023-07-20 15:53:19 +0200

    Migration de la BDD - Rajout de la table Inscription - Rajout de la table CréneauInscriptionEnfant - Rajout de la table CréneauInscriptionEquipier

--- sitecaroubiers/plannings/models.py
+++ sitecaroubiers/plannings/models.py
@@ -11,6 +11,53 @@ class Family(models.Model):
     def __str__(self):
         return f"{self.name}"
 
+# --------------------------------- INSCRIPTIONS
+class Jour(models.TextChoices):
+    LUNDI = 'LUN'
+    MARDI = 'MAR'
+    MERCREDI = 'MER'
+    JEUDI = 'JEU'
+    VENDREDI = 'VEN'
+class Creneau(models.TextChoices):
+    MATIN = 'MATIN'
+    MIDI = 'MIDI'
+    SOIR = 'SOIR'
+
+# Une inscription est enregistrée lorsque le parent valide soit sur la page
+# d'inscription périsco "enfant" soit sur la page "équipier."
+# Les créneaux auxquels correspond l'inscription sont enregistrés dans 2 tables distinctent
+# qui référencent directement l'inscription. Cela permet d'éviter les redondances
+class Inscription(models.Model):
+    prenom = models.CharField(max_length=50)
+    famille = models.ForeignKey(Family, on_delete=models.SET_NULL, null=True)
+    datetime = models.DateTimeField(auto_now=True)
+    commentaire = models.CharField(max_length=1500, blank=True)
+
+class CreneauInscriptionEnfant(models.Model):
+    
+    class Cycle(models.TextChoices):
+        CYCLE_1 = 'C1'
+        CYCLE_2 = 'C2'
+        CYCLE_3 = 'C3'
+        COLLEGE = 'COL'
+    cycle_enfant = models.CharField(choices=Cycle.choices, max_length=10)
+    periode = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    semaine = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(52)])
+    jour = models.CharField(choices=Jour.choices, max_length=15)
+    creneau = models.CharField(choices=Creneau.choices, max_length=10)
+    horaire_arrivee = models.TimeField()
+    horaire_depart = models.TimeField()
+    id_inscription = models.ForeignKey(Inscription, on_delete=models.CASCADE)
+
+class CreneauInscriptionEquipier(models.Model):
+    periode = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    semaine = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(52)])
+    jour = models.CharField(choices=Jour.choices, max_length=15)
+    creneau = models.CharField(choices=Creneau.choices, max_length=10)
+    id_inscription = models.ForeignKey(Inscription, on_delete=models.CASCADE)
+
+# --------------------------------- FIN INSCRIPTIONS
+
 # class Periode(models.Model):
 
 #     name = models.CharField(max_length=50, unique=True)
